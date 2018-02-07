1b533f079c3d45fec3c757a049b3632056e8800b
john josua paderon <johnjosua.paderon@outlook.com>
2018-02-07 10:38:08 +0800

    Added Sream conversion.

---
+++ Sorschia/Utilities/ValueConverter.Part.Stream.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+namespace Sorschia.Utilities
+{
+    partial class ValueConverter
+    {
+        /// <summary>
+        /// Converts <see cref="object"/> to <see cref="Stream"/>
+        /// </summary>
+        public static Stream ToStream(object value)
+        {
+            return value == null ? null : ConvertBase(value, val => new MemoryStream(ToByteArray(val)));
+        }
+    }
+}
