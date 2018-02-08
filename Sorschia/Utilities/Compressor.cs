75f2a6824731e6c90ab3666e8e5c92dd265f1127
john josua paderon <johnjosua.paderon@outlook.com>
2018-02-09 00:09:04 +0800

    data compression utility

---
+++ Sorschia/Utilities/Compressor.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using System.IO.Compression;
+
+namespace Sorschia.Utilities
+{
+    /// <summary>
+    /// Exposes functions that compress and decompress <see cref="byte"/>[]
+    /// </summary>
+    public static class Compressor
+    {
+        /// <summary>
+        /// Compresses the <see cref="byte"/>[]
+        /// </summary>
+        public static byte[] Compress(byte[] uncompressed)
+        {
+            var output = new MemoryStream();
+
+            using (var deflateStream = new DeflateStream(output, CompressionLevel.Optimal))
+            {
+                deflateStream.Write(uncompressed, 0, uncompressed.Length);
+            }
+
+            return output.ToArray();
+        }
+
+        /// <summary>
+        /// Decompresses the <see cref="byte"/>[]
+        /// </summary>
+        public static byte[] Decompress(byte[] compressed)
+        {
+            var input = new MemoryStream(compressed);
+            var output = new MemoryStream();
+
+            using (var deflateStream = new DeflateStream(input, CompressionMode.Decompress))
+            {
+                deflateStream.CopyTo(output);
+            }
+
+            return output.ToArray();
+        }
+    }
+}
