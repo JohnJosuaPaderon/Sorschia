5a8c97fb0805028ae9a343bb90d359663c2d6b57
john josua paderon <johnjosua.paderon@outlook.com>
2018-02-11 13:10:50 +0800

    Base-class for managing database entities.

---
+++ Sorschia/Manager/DbEntityManagerBase.cs
@@ -0,0 +1,17 @@
+﻿using Sorschia.Data;
+using Sorschia.Entity;
+using Sorschia.Process;
+
+namespace Sorschia.Manager
+{
+    public abstract class DbEntityManagerBase<T, TId> : DbManagerBase
+        where T : IEntity<TId>
+    {
+        public DbEntityManagerBase(IProcessContextManager contextManager, IConnectionStringPool connectionStringPool) : base(contextManager, connectionStringPool)
+        {
+            _Source = new EntityCollection<T, TId>();
+        }
+
+        protected readonly IEntityCollection<T, TId> _Source;
+    }
+}
