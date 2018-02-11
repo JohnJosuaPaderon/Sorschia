25f4b07b90b3e62db81716aa0aaf004be0f53760
john josua paderon <johnjosua.paderon@outlook.com>
2018-02-11 13:12:54 +0800

    Added ProcessProvider static class. Registering and providing concrete implementation of IProcessCore, IProcess, IProcess<T>

---
+++ Sorschia/Process/ProcessProvider.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Sorschia.Process
+{
+    public static class ProcessProvider
+    {
+        static ProcessProvider()
+        {
+            _Services = new ServiceCollection();
+        }
+
+        private static readonly IServiceCollection _Services;
+        private static IServiceProvider ServiceProvider;
+
+        public static void Build()
+        {
+            ServiceProvider = _Services.BuildServiceProvider();
+        }
+
+        public static void Register<TContract, TImplementation>()
+            where TContract : class, IProcessCore
+            where TImplementation : class, TContract
+        {
+            _Services.AddTransient<TContract, TImplementation>();
+        }
+
+        public static T Get<T>()
+            where T : IProcessCore
+        {
+            return ServiceProvider.GetService<T>();
+        }
+    }
+}
