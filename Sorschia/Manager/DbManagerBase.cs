141370e6d5a25fbe93b90191e9ebf044a0da5a5e
john josua paderon <johnjosua.paderon@outlook.com>
2018-02-11 13:10:31 +0800

    Added base-class for managing database models.

---
+++ Sorschia/Manager/DbManagerBase.cs
@@ -0,0 +1,25 @@
+﻿using Sorschia.Data;
+using Sorschia.Process;
+using System.Security;
+
+namespace Sorschia.Manager
+{
+    public abstract class DbManagerBase
+    {
+        public DbManagerBase(IProcessContextManager contextManager, IConnectionStringPool connectionStringPool)
+        {
+            _ContextManager = contextManager;
+            _ConnectionStringPool = connectionStringPool;
+        }
+
+        private readonly IConnectionStringPool _ConnectionStringPool;
+        private readonly IProcessContextManager _ContextManager;
+
+        protected IProcessContext InitializeContext(SecureString secureConnectionString)
+        {
+            var context = _ContextManager.Initialize();
+            _ConnectionStringPool.Add(context, secureConnectionString);
+            return context;
+        }
+    }
+}
