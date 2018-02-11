d6e76ccfa886a823d955b0ff8d3281757b545bc0
john josua paderon <johnjosua.paderon@outlook.com>
2018-02-11 13:09:52 +0800

    Added base-class for managing entities.

---
+++ Sorschia/Entity/EntityManagerBase.cs
@@ -0,0 +1,12 @@
+﻿namespace Sorschia.Entity
+{
+    public abstract class EntityManagerBase<T, TIdentifier> where T : IEntity<TIdentifier>
+    {
+        public EntityManagerBase()
+        {
+            _Source = new EntityCollection<T, TIdentifier>();
+        }
+
+        protected readonly IEntityCollection<T, TIdentifier> _Source;
+    }
+}
