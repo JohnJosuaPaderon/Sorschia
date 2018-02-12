f8f6ce3bc6e6ee9363339202304d169a08d31ae0
john josua paderon <johnjosua.paderon@outlook.com>
2018-02-12 23:18:04 +0800

    Helper methods for adding, updating and removing entities from source.

--- Sorschia/Manager/DbEntityManagerBase.cs
+++ Sorschia/Manager/DbEntityManagerBase.cs
@@ -1,6 +1,8 @@
 ﻿using Sorschia.Data;
 using Sorschia.Entity;
 using Sorschia.Process;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Sorschia.Manager
 {
@@ -13,5 +15,74 @@ namespace Sorschia.Manager
         }
 
         protected readonly IEntityCollection<T, TId> _Source;
+
+        protected T TryAdd(T entity)
+        {
+            if (!Equals(entity, default(T)))
+            {
+                _Source.Add(entity);
+            }
+
+            return entity;
+        }
+
+        protected IEnumerable<T> TryAdd(IEnumerable<T> entities)
+        {
+            if (entities != null && entities.Any())
+            {
+                foreach (var entity in entities)
+                {
+                    TryAdd(entity);
+                }
+            }
+
+            return entities;
+        }
+
+        protected T TryUpdate(T entity)
+        {
+            if (!Equals(entity, default(T)))
+            {
+                _Source[entity.Id] = entity;
+            }
+
+            return entity;
+        }
+
+        protected IEnumerable<T> TryUpdate(IEnumerable<T> entities)
+        {
+            if (entities != null && entities.Any())
+            {
+                foreach (var entity in entities)
+                {
+                    TryUpdate(entity);
+                }
+            }
+
+            return entities;
+        }
+
+        protected T TryRemove(T entity)
+        {
+            if (!Equals(entity, default(T)))
+            {
+                _Source.Remove(entity);
+            }
+
+            return entity;
+        }
+
+        protected IEnumerable<T> TryRemove(IEnumerable<T> entities)
+        {
+            if (entities != null && entities.Any())
+            {
+                foreach (var entity in entities)
+                {
+                    TryRemove(entity);
+                }
+            }
+
+            return entities;
+        }
     }
 }
