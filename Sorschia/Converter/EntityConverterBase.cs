7ac1d0e5198eb1fa4374050c779fe05ea6392b88
john josua paderon <johnjosua.paderon@outlook.com>
2018-02-12 23:16:29 +0800

    Added Converter base-class for entities.

---
+++ Sorschia/Converter/EntityConverterBase.cs
@@ -0,0 +1,21 @@
+﻿using Sorschia.Data;
+using Sorschia.Entity;
+
+namespace Sorschia.Converter
+{
+    public abstract class EntityConverterBase<T, TId> : DbDataReaderConverterBase<T>
+        where T : IEntity<TId>
+    {
+        public EntityConverterBase()
+        {
+            Id = new DbDataReaderConverterProperty<TId>();
+        }
+
+        public DbDataReaderConverterProperty<TId> Id { get; }
+
+        public virtual void Reset()
+        {
+            Id.Reset();
+        }
+    }
+}
---
+++ Sorschia/Converter/IEntityConverter.cs
@@ -0,0 +1,11 @@
+﻿using Sorschia.Data;
+using Sorschia.Entity;
+
+namespace Sorschia.Converter
+{
+    public interface IEntityConverter<T, TId> : IDbDataReaderConverter<T>
+        where T : IEntity<TId>
+    {
+        DbDataReaderConverterProperty<TId> Id { get; }
+    }
+}
