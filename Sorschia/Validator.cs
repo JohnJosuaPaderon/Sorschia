e805deae02089dddfdbcab4d5245580cc3ea5282
john josua paderon <johnjosua.paderon@outlook.com>
2018-03-09 11:28:23 +0800

    Generic methods of LessThan, LessThanOrEqual, GreaterThan, GreaterThanOrEqual, InCollection and NotInCollection

--- Sorschia/Validator.cs
+++ Sorschia/Validator.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Sorschia
@@ -37,6 +38,68 @@ namespace Sorschia
             {
                 throw new ValidationException(message, ValidationType.EmptyIEnumerable);
             }
+        }
+
+        public static void LessThan<T>(T value, T minValue, string message)
+            where T : struct, IComparable<T>
+        {
+            if (value.CompareTo(minValue) < 0)
+            {
+                throw new ValidationException(message, ValidationType.LessThan);
+            }
+        }
+
+        public static void LessThanOrEqual<T>(T value, T minValue, string message)
+            where T : struct, IComparable<T>
+        {
+            if (value.CompareTo(minValue) <= 0)
+            {
+                throw new ValidationException(message, ValidationType.LessThan);
+            }
+        }
+
+        public static void GreaterThan<T>(T value, T maxValue, string message)
+            where T : struct, IComparable<T>
+        {
+            if (value.CompareTo(maxValue) > 0)
+            {
+                throw new ValidationException(message, ValidationType.GreaterThan);
+            }
+        }
+
+        public static void GreaterThanOrEqual<T>(T value, T maxValue, string message)
+            where T : struct, IComparable<T>
+        {
+            if (value.CompareTo(maxValue) >= 0)
+            {
+                throw new ValidationException(message, ValidationType.GreaterThanOrEqual);
+            }
+        }
+
+        public static void InCollection<T>(T value, IEnumerable<T> collection, Func<T, T, bool> equals, string message)
+        {
+            if (collection.Any(item => equals(value, item)))
+            {
+                throw new ValidationException(message, ValidationType.InCollection);
+            }
+        }
+
+        public static void InCollection<T>(T value, IEnumerable<T> collection, string message)
+        {
+            InCollection(value, collection, (param1, param2) => Equals(param1, param2), message);
+        }
+
+        public static void NotInCollection<T>(T value, IEnumerable<T> collection, Func<T, T, bool> equals, string message)
+        {
+            if (!collection.Any(item => equals(value, item)))
+            {
+                throw new ValidationException(message, ValidationType.NotInCollection);
+            }
+        }
+
+        public static void NotInCollection<T>(T value, IEnumerable<T> collection, string message)
+        {
+            NotInCollection(value, collection, (param1, param2) => Equals(param1, param2), message);
         }
     }
 }
